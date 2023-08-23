5d6884173d1e2e3581d6c8bd0b37fa2425e35dd5
tjddnr7272@hanmail.net <tjddnr7272@hanmail.net>
2023-08-23 12:56:45 +0900

    rr

--- src/main/java/com/runningmate/domain/board/dto/ReplyDto.java
+++ src/main/java/com/runningmate/domain/board/dto/ReplyDto.java
@@ -15,61 +15,7 @@ public class ReplyDto {
     private int replyNoticeId;
     private String email; // 이메일 필드 추가
 
-    
-    public ReplyDto() {}
-    
-	public ReplyDto(int replyId, String replyContent, Date replyDate, int replyNoticeId) {
-		this.replyId = replyId;
-		this.replyContent = replyContent;
-		this.replyDate = replyDate;
-		this.replyNoticeId = replyNoticeId;
-	}
-	
-	public ReplyDto(String replyName) {
-		this.replyName = replyName;
-	}
 
-	public int getReplyId() {
-		return replyId;
-	}
-
-	public void setReplyId(int replyId) {
-		this.replyId = replyId;
-	}
-	
-
-	public String getReplyContent() {
-		return replyContent;
-	}
-
-	public void setReplyContent(String replyContent) {
-		this.replyContent = replyContent;
-	}
-
-	public Date getReplyDate() {
-		return replyDate;
-	}
-
-	public void setReplyDate(Date replyDate) {
-		this.replyDate = replyDate;
-	}
-
-	public int getReplyNoticeId() {
-		return replyNoticeId;
-	}
-
-	public void setReplyNoticeId(int replyNoticeId) {
-		this.replyNoticeId = replyNoticeId;
-	}
-
-	public String getReplyName() {
-		return replyName;
-	}
-
-	public void setReplyName(String replyName) {
-		this.replyName = replyName;
-	}
-	
 	
 	
 }
